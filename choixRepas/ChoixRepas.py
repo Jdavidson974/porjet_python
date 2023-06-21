45b47e570e61ed4535885565b03c7fbd572630de
JD <jd9743101@gmail.com>
2023-06-21 20:39:28 +0200

    apiUrl choix repas

--- choixRepas/ChoixRepas.py
+++ choixRepas/ChoixRepas.py
@@ -1,10 +1,13 @@
 from tkinter import *
-class ChoixRepas() :
-    def __init__(self,canvas:Canvas) -> None:
+
+
+class ChoixRepas:
+    apiUrl = "https://www.themealdb.com/api/json/v1/1/random.php"
+
+    def __init__(self, canvas: Canvas) -> None:
         self.canvas = canvas
-        # Build la page choix des repas 
-    def buildRepasPage(self) :
-        title = Label(self.canvas,text="Choix des repas",bg="#FEFEFE")
+        # Build la page choix des repas
+
+    def buildRepasPage(self):
+        title = Label(self.canvas, text="Choix des repas", bg="#FEFEFE")
         title.pack()
-        
-        
